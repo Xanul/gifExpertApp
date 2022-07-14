03e66f88ce5bf3686e9a3f427522fdd93209e57c
Xanul <rivas.060@gmail.com>
2022-07-14 14:45:22 -0500

    Created the GifExpertApp and added the categories hook

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,27 @@
-
+import {useState} from 'react';
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Pokemon']);
+  
+  const onAddCategory = () => {
+    setCategories([...categories, 'Valorant']);
+    // setCategories(cat => [...cat, 'Dragon Ball']);
+  }
+
   return (
     <>
+      {/* Titulo */}
+
       <h1>GifExpertApp</h1>
+      {/* Input */}
+
+      {/* Listado de gifs */}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map(category => <li key={ category }>{category}</li>)}
+      </ol>
+        {/* Gif Item */}
     </>
   )
 }
