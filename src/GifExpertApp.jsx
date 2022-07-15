9f4bc992d6ccad5750f96d8a4e5c8152f0d0c64e
Xanul <rivas.060@gmail.com>
2022-07-15 12:56:43 -0500

    Added the logic to evaluate if the requested input already exist in array

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,6 +6,9 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState([]);
   
   const onAddCategory = (newCategory) => {
+
+    if (categories.includes(newCategory)) return;
+
     setCategories([newCategory, ...categories])
 
     // setCategories([...categories, newCategory]);
