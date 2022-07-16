fc5dee7c885c85a9f6a402d926e28fc1da974f0a
Xanul <rivas.060@gmail.com>
2022-07-16 18:13:12 -0500

    Added the component index to centralize the exports

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from "react";
-// import { getGifs } from "../helpers/getGifs";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
-
-
 
 export const GifGrid = ({category}) => {
   
--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,33 +1,22 @@
 import {useState} from 'react';
-import { AddCategory } from './Components/AddCategory';
-import { GifGrid } from './Components/GifGrid';
-
+import { AddCategory, GifGrid } from './Components';
 
 export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState([]);
   
   const onAddCategory = (newCategory) => {
-
     if (categories.includes(newCategory)) return;
-
     setCategories([newCategory, ...categories])
-
-    // setCategories([...categories, newCategory]);
-    // setCategories(cat => [...cat, 'Dragon Ball']);
   }
 
   return (
     <>
-
       <h1>GifExpertApp</h1>
-
       <AddCategory 
         onNewCategory={event => onAddCategory(event)}
       />
-
       {categories.map(category => <GifGrid key={category} category={category} />)}
-      
     </>
   )
 }
