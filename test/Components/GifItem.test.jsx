30519014ecc019f2779ef2e3bdfb6e068705d575
Xanul <rivas.060@gmail.com>
2022-07-16 20:01:26 -0500

    Added tests for GifItem component

--- test/Components/GifItem.test.jsx
+++ test/Components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/Components/GifItem"
 
 
@@ -10,9 +10,26 @@ describe('Probando el componente GifItem', () => {
   test('Debe de hacer match con el snapshot', () => {
 
    const {container} = render (<GifItem title={title} url={url}/>);
-
    expect(container).toMatchSnapshot();
 
   });
+
+  test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+
+    render (<GifItem title={title} url={url}/>);
+    // screen.debug();
+    // expect(screen.getByRole('img').src).toBe(url);
+
+    const {src , alt} = screen.getByRole('img');
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test('Debe de mostrar el titulo en el componente', () => {
+    
+    render (<GifItem title={title} url={url}/>);
+    expect(screen.getByText(title)).toBeTruthy();
+
+  })
 
 })
